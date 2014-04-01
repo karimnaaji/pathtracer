a31c03086545e4df82efd4b57f942270b095f312
Karim Naaji <karim.naaji@gmail.com>
2014-04-01 23:37:06 +0200

    clean project

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "renderer.h"
 #include "sceneparser.h"
 #include "scene.h"
-#include "bvh.h"
 
 int main(int argc, char** argv) {
     if(argc < 3) {
@@ -16,15 +15,8 @@ int main(int argc, char** argv) {
 
     SceneParser parser(scenefile);
     Scene *scene = parser.BuildScene();
-    //BVH bvh(scene->Objects());
-    //bvh.Display(bvh.root);
 
     PPMImage *film = scene->GetCamera()->Film();
-
-    float data[film->GetWidth()*film->GetHeight()*3];
-    for(int i = 0; i < film->GetWidth() * film->GetHeight() * 3; ++i) {
-        data[i] = i;
-    }
 
     Renderer renderer(atoi(argv[2]));
     renderer.Init(*film);
